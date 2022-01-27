e6d3e31b17bf1d0798ac88baeb01f3efd9030b38
Giovana Thais <giovana.carvalho@ma9.com.br>
2022-01-27 14:24:39 -0300

    first commit

---
+++ autenticacao/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from autenticacao import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+#fazendo rotas de login e cadastro
+urlpatterns = [
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('logar/', views.logar, name='logar'),
+    path('sair/', views.sair, name="sair"),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
